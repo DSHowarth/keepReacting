606e981c90dabae5c2f6f543bc1879a9e8e5fd96
Dash <dkshowarth@gmail.com>
2023-09-08 16:52:53 -0700

    create remove puzzle and increase score reducer

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -12,5 +12,24 @@ import {
                 puzzles: [...state.puzzles, action.payload]
             }
         }
+
+        case REMOVE_PUZZLE: {
+            return {
+                ...state,
+                puzzles: [...state.puzzles].filter(
+                (puzzle) => puzzle.id !== action.payload
+                ),
+            };
+        }
+        
+        case INCREASE_SCORE: {
+
+            const newScore = state.points + action.payload
+            
+            return {
+                ...state,
+                points: newScore 
+            }
+        }
     }
   }
