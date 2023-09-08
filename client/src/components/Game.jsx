97cb295e608217d947f3d63516a717c992cf3c9e
Dash <dkshowarth@gmail.com>
2023-09-08 13:04:22 -0700

    add puzzle generation logic, but not the working functions yet, to the game loop

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,5 +1,7 @@
 import { useGameContext } from '../utils/GameContext';
 
+// TODO: Create generatePuzzle(), create puzzleGen()
+
 export default function Game () {
     // bring in our game context
     const [state, dispatch] = useGameContext();
@@ -10,18 +12,26 @@ export default function Game () {
     const [gameActive, setGameActive] = useState(true);
 
     useEffect( () => {
+        
+        // generate 5 puzzles on game start
+        for(let i = 0; i < 5; i++) {
+            generatePuzzle();
+        }
+        // interval updates every second, checking to see if the player has run out of time 
+        // while the game goes on, the points total increases every second 
         const timerInterval = setInterval( () => {
             if (timeRemaining > 0) {
                 setTimeRemaining(timeRemaining--);
                 dispatch({ type: INCREASE_SCORE, payload: pointIncrement });
+                if (puzzleGen()) {
+                    generatePuzzle();
+                }
             }
             else {
                 clearInterval(timerInterval);
                 setGameActive(false);
             }
-        }
-
-        )
+        }, 1000)
     }, [])
 
     if (gameActive) {
