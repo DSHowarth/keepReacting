20a1885d734e504d59693ced50bb31591fe2c15e
Xiaoran <xc2422@columbia.edu>
2023-09-08 11:31:57 -0700

    type devs

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,14 +1,17 @@
 const typeDefs = `
   type User {
-
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
   }
 
-  type Book {
-   
-  }
-
-  input BookInput{ 
-   
+  type Score {
+    _id: ID!
+    date: Int
+    score: Int
+    teammates: String
+    userId: ID!
   }
 
   type Auth {
@@ -17,11 +20,21 @@ const typeDefs = `
   }
 
   type Query {
+    score: [Score]
+  }
  
+  input ScoreInput{
+    _id: ID
+    date: Int
+    score: Int
+    teammates: String
+    userId: ID!
   }
 
   type Mutation {
-  
+    addUser(username: String!, email: String!, password: String!): Auth
+    logIn(email: String!, password: String!): Auth  
+    score(input: ScoreInput!): Score
   }
 `;
 
