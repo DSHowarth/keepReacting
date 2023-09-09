e5718973291ff3ea2d0240d7478749c9cb2e836e
Xiaoran <xc2422@columbia.edu>
2023-09-09 12:52:26 -0700

    resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,47 @@
+const { Score, User } = require("../models");
 
+const resolvers = {
+  Query: {
+    Score: async () => {
+      return await Score.find();
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      return await User.create({ username, email, password });
+    },
+
+    addScore: async (parent, { score }, context) => {
+      if (context.user) {
+        const score = await Score.create({
+          score,
+          userId: context.user._id,
+        });
+
+        return score;
+      }
+      throw AuthenticationError;
+      ("You need to be logged in!");
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
