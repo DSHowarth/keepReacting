38d83dd21b997f5a33de57b5cf7f31db44b76597
Xiaoran <xc2422@columbia.edu>
2023-09-10 11:23:04 -0700

    the manual

--- client/src/pages/Manual.jsx
+++ client/src/pages/Manual.jsx
@@ -9,6 +9,41 @@ const Manual = () => {
     setSelectedColor(color);
   };
 
+  const greek = "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩσς";
+  const alphabet = "ZKFYPEXTRNGQLUAMSIJBCODWVH";
+
+  const tableStyle = {
+    border: "1px solid #000",
+    borderCollapse: "collapse",
+    width: "100%",
+  };
+
+  const headerCellStyle = {
+    border: "1px solid #000",
+    backgroundColor: "purple",
+    color: "white",
+    padding: "8px",
+  };
+
+  const dataCellStyle = {
+    border: "1px solid #000",
+    padding: "8px",
+  };
+
+  // Create an array of rows based on the greek and alphabet strings
+  const rows = [];
+  for (let i = 0; i < greek.length; i++) {
+    const greekLetter = greek[i];
+    const alphabetLetter = alphabet[i];
+
+    rows.push(
+      <tr key={i}>
+        <td style={dataCellStyle}>{greekLetter}</td>
+        <td style={dataCellStyle}>{alphabetLetter}</td>
+      </tr>
+    );
+  }
+
   return (
     <div>
       <h1>Game Manual</h1>
@@ -16,12 +51,16 @@ const Manual = () => {
       {/* Cypher Table */}
       <div>
         <h2>Cypher Table</h2>
-        <textarea
-          rows="5"
-          cols="30"
-          value={cypherTable}
-          onChange={(e) => setCypherTable(e.target.value)}
-        />
+        {/* Table with header cell and data cell styles */}
+        <table style={tableStyle}>
+          <thead>
+            <tr>
+              <th style={headerCellStyle}>Greek</th>
+              <th style={headerCellStyle}>Alphabet</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
       </div>
     </div>
   );
