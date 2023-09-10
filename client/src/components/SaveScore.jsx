f2d31e5411ef991cb2b8030fb30173e428eba16a
Dash <dkshowarth@gmail.com>
2023-09-10 10:26:27 -0700

    create savescore input display functionality

--- client/src/components/SaveScore.jsx
+++ client/src/components/SaveScore.jsx
@@ -1,10 +1,16 @@
-import ADD_SCORE from '../utils/mutations'
-
+import ADD_SCORE from '../utils/mutations';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 export default function SaveScore ({ points }) {
 
-    
+    const [teammates, setTeammates] = useState('')
 
+    const updateTeammates = (event) => {
+        const { value } = event.target;
+        setTeammates(value);
+    }
 
     return (
         <>
@@ -13,7 +19,7 @@ export default function SaveScore ({ points }) {
                 Would you like to save your score?</p>
                 <Form.Group>
                     <Form.Label>Teammates</Form.Label>
-                    <Form.Control type='text'/>
+                    <Form.Control size="lg" type='text' onChange={updateTeammates} value={teammates}/>
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
