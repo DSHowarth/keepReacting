405b06dc6b05f68cbb15d34957818e0c9fe80114
Timothy <timsu100@gmail.com>
2023-09-09 18:32:18 -0700

    add scores page

--- client/src/pages/Scores.jsx
+++ client/src/pages/Scores.jsx
@@ -1,5 +1,20 @@
-
+import { QUERY_SCORES } from "../utils/queries";
+import { useQuery } from '@apollo/client'
 
 export default function Scores() {
+  const { data } = useQuery(QUERY_SCORES);
 
+  return (
+    <>
+      <h2>Top Scores</h2>
+      {data ? (data.scores.map((score) => (
+        <div className="flex-row" key={score._id}>
+          <h3>{score.user.username}: </h3>
+          <h3>{score.score}</h3>
+          <h3>With help from {score.teammates}</h3>
+        </div>
+      ))
+      ) : null}
+    </>
+  )
 }
