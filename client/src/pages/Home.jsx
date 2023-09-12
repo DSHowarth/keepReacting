fe2c36c5a4c6335bd752af4e186e91bbf7e64a00
Dash <dkshowarth@gmail.com>
2023-09-11 20:14:48 -0700

    implement home page login message

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { LinkContainer } from "react-router-bootstrap";
+import Auth from '../utils/auth';
 
 export default function Home() {
   return (
@@ -10,16 +11,21 @@ export default function Home() {
           whileHover={{ scale: 1.2 }}
           whileTap={{ scale: 0.8 }}
           type="submit"
+          disabled={!Auth.loggedIn()}
+          className={'playButton'}
         >
           Play
         </motion.button>
       </LinkContainer>
+      {!Auth.loggedIn() && 
+        <p>Please log in to play the game. Viewing the manual can be done by anyone, though!</p>}
       <br />
       <LinkContainer to="/manual">
         <motion.button
           whileHover={{ scale: 1.2 }}
           whileTap={{ scale: 0.8 }}
           type="submit"
+          className={'manualButton'}
         >
           Manual
         </motion.button>
